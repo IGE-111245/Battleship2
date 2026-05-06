913b898a91d9e082dcb9423ce396b6cdfe7b42c3
PedroAngelino <103763819+PedroAngelino@users.noreply.github.com>
2026-05-07 00:30:07 +0100

    Refactoring e refactoringoppurtunities

--- src/main/java/battleship/FogGUI.java
+++ src/main/java/battleship/FogGUI.java
@@ -9,39 +9,43 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class FogGUI extends Application {
-    private boolean[][] revelado = new boolean[10][10];
 
+    
+    public static final int windowsize = 500;
+    public static final int cellsize = 50;
+    private boolean[][] revelado = new boolean[10][10];
+    
     @Override
     public void start(Stage stage) {
-        Canvas canvas = new Canvas(500, 500);
+        Canvas canvas = new Canvas(FogGUI.windowsize, FogGUI.windowsize);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         desenhaTabuleiro(gc);
 
         canvas.setOnMouseClicked(e -> {
-            int x = (int)(e.getX() / 50);
-            int y = (int)(e.getY() / 50);
+            int x = (int)(e.getX() / cellsize);
+            int y = (int)(e.getY() / FogGUI.cellsize);
             revela(x, y, gc);
         });
 
         StackPane root = new StackPane();
         root.getChildren().add(canvas);
-        stage.setScene(new Scene(root, 500, 500));
+        stage.setScene(new Scene(root, FogGUI.windowsize, FogGUI.windowsize));
         stage.setTitle("Battleship Fog of War");
         stage.show();
     }
 
     private void desenhaTabuleiro(GraphicsContext gc) {
         gc.setFill(Color.LIGHTBLUE);
-        gc.fillRect(0, 0, 500, 500);
+        gc.fillRect(0, 0, FogGUI.windowsize, FogGUI.windowsize);
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(1);
         for (int i = 0; i <= 10; i++) {
-            gc.strokeLine(i*50, 0, i*50, 500);
-            gc.strokeLine(0, i*50, 500, i*50);
+            gc.strokeLine(i* FogGUI.cellsize, 0, i* FogGUI.cellsize, FogGUI.windowsize);
+            gc.strokeLine(0, i* FogGUI.cellsize, FogGUI.windowsize, i* FogGUI.cellsize);
         }
         // Nevoeiro
         gc.setFill(new Color(0.3, 0.3, 0.3, 0.8));
-        gc.fillRect(0, 0, 500, 500);
+        gc.fillRect(0, 0, FogGUI.windowsize, FogGUI.windowsize);
     }
 
     private void revela(int x, int y, GraphicsContext gc) {
@@ -50,7 +54,7 @@ public class FogGUI extends Application {
         // Simula hit/miss (liga à tua Fleet depois)
         boolean hit = Math.random() > 0.8;
         gc.setFill(hit ? Color.RED : Color.BLUE);
-        gc.fillRect(x*50 + 5, y*50 + 5, 40, 40);
+        gc.fillRect(x* FogGUI.cellsize + 5, y* FogGUI.cellsize + 5, 40, 40);
         System.out.println("Tiro (" + x + "," + y + ") - " + (hit ? "Hit!" : "Miss"));
         
     }
