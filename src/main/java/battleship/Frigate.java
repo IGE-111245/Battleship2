bfe0e72ba7a787641258621c2f5246ad31fe5255
VicenteZV <vzvaa@iscte-iul.pt>
2026-05-06 15:02:18 +0100

    Refactoring: Extract Method — fillVertical and fillHorizontal in Frigate

--- src/main/java/battleship/Frigate.java
+++ src/main/java/battleship/Frigate.java
@@ -21,21 +21,27 @@ public class Frigate extends Ship {
 
 		switch (bearing) {
 			case NORTH:
-				for (int r = 0; r < this.getSize(); r++)
-					getPositions().add(new Position(pos.getRow() + r, pos.getColumn()));
-				break;
+                fillVertical(pos);
+                break;
 			case SOUTH:
-				for (int r = 0; r < this.getSize(); r++)
-					getPositions().add(new Position(pos.getRow() + r, pos.getColumn()));
-				break;
+                fillVertical(pos);
+                break;
 			case EAST:
-				for (int c = 0; c < this.getSize(); c++)
-					getPositions().add(new Position(pos.getRow(), pos.getColumn() + c));
-				break;
+                fillHorizontal(pos);
+                break;
 			case WEST:
-				for (int c = 0; c < this.getSize(); c++)
-					getPositions().add(new Position(pos.getRow(), pos.getColumn() + c));
-				break;
+                fillHorizontal(pos);
+                break;
 		}
 	}
+
+    private void fillHorizontal(IPosition pos) {
+        for (int c = 0; c < this.getSize(); c++)
+            getPositions().add(new Position(pos.getRow(), pos.getColumn() + c));
+    }
+
+    private void fillVertical(IPosition pos) {
+        for (int r = 0; r < this.getSize(); r++)
+            getPositions().add(new Position(pos.getRow() + r, pos.getColumn()));
+    }
 }
